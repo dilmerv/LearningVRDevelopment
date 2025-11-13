e3ddf221060d1279f3d1c2299a2c370f83c98091
Dilmer Valecillos <dilmervst@gmail.com>
2025-11-13 13:23:12 -0700

    Added Passthrough, Grabbale Objects, and MRUK

---
+++ Assets/Scripts/TestingMetaInspector.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class TestingMetaInspector : MonoBehaviour
+{
+    private float mugFallSpeed = 29.0f;
+
+    private Vector3 mugPositionRelativeToTheGuitar = new Vector3(1, 2, 3);
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
